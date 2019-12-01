d64cb02701da5b9ce9fc357dad8fd894794b8750
stanle-cs <stanle-cs@users.noreply.github.com>
2019-12-01 16:37:32 -0600

    try to get the ball moving

--- ncursespong.c
+++ ncursespong.c
@@ -47,17 +47,15 @@ void processKeystroke(int c, int* x, int* y, Object *paddle) {
             (void) mvwprintw(stdscr, *y, *x + 1, "%s", player);
             *x += 1;
             break;
-        */
-    paddle->x = *x;
-    paddle->y = *y;
-    
+        */    
     }
 }
+int maxX, maxY;
 
 static void ncursesInit() {
     /* arrange interrupts to terminate */
-
     (void) initscr();      /* initialize the curses library */
+    box(stdscr, 0, 0);
     keypad(stdscr, TRUE);  /* enable keyboard mapping */
     (void) nonl();         /* tell curses not to do NL->CR/NL on output */
     (void) nodelay(stdscr, 1);  /* tell ncurses to trhow ERR if getch() waits too long
@@ -91,25 +89,54 @@ bool checkCollision(Object paddle, Object ball) {
     else return false;
 }
 
+int vX = 1, vY = 1;
+void moveBall(int maxX, int maxY) {
+    mvprintw(ball.x, ball.y, ball.modelDel);
+    if ((ball.x + vX > maxX) || (ball.x + vX < 0)) {
+        vX = -vX;
+    }
+    ball.x += vX;
+    if ((ball.y + vY > maxY) || (ball.y + vY < 0)) {
+        vY = -vY;
+    }
+    ball.y += vY;
+
+    if (ball.x > maxX) ball.x = maxX;
+    if (ball.x < 0) ball.x = 0;
+    if (ball.y > maxY) ball.y = maxY;
+    if (ball.y < 0) ball.y = 0;
+
+    mvprintw(ball.x, ball.y, ball.model);
+}
 
 int main(int argc, char *argv[]) {
     
     int num = 0;
     int currentX = 0, currentY = 0;
+    usleep(3000000);
     ncursesInit();
+    
     /* initialize your non-curses data structures here */
     for (;;)
     {
-        usleep(3000);
+        usleep(100000);
+        getmaxyx(stdscr, maxY, maxX);
         int c = getch();     /* refresh, accept single keystroke of input */
+        moveBall(maxX, maxY);
         attrset(COLOR_PAIR(num % 8));
         num++;
         mvprintw(20, 20, "%d", num);
+        mvprintw(21, 20, "                                            ");
         mvprintw(21, 20, "%d %d - %d %d ", paddle.x, paddle.y, currentX, currentY);
         /* process the command keystroke */
         if (c != ERR) {
             processKeystroke(c, &currentX, &currentY, &paddle);
-            if (checkCollision(paddle, ball)) mvwprintw (stdscr, 22, 20, "hit! %d %d - %d %d", paddle.x, paddle.y, ball.x, ball.y);
+ 
+            paddle.x = currentX;
+            paddle.y = currentY;
+            if (checkCollision(paddle, ball)) mvwprintw (stdscr, 23, 20, "hit!");
+            mvprintw(22, 20, "                                            ");
+            mvwprintw (stdscr, 22, 20, "paddle: %d %d - ball: %d %d", paddle.x, paddle.y, ball.x, ball.y);
             if (c == 'Q') break;
         }
     }
